2b4ddef1d8c0b7239c30eec6b97d28f0701b4e7a
Cyprian Zasada <120047588+cyptrix12@users.noreply.github.com>
2025-04-04 16:06:16 +0200

    Cleared comments

--- src/audio.rs
+++ src/audio.rs
@@ -13,14 +13,12 @@ pub fn run_audio_processing(
 ) {
     let host = cpal::default_host();
 
-    // Wybór domyślnego mikrofonu
     let device = host
         .default_input_device()
         .expect("Nie znaleziono mikrofonu");
 
     let config = device.default_input_config().unwrap();
 
-    // Wprost z dokumentacji
     let stream = match config.sample_format() {
         cpal::SampleFormat::F32 => run_stream::<f32>(
             &device,
@@ -48,10 +46,8 @@ pub fn run_audio_processing(
         ),
     };
 
-    // Uruchomienie strumienia
     stream.play().unwrap();
 
-    // Utrzymanie wątku przy życiu
     loop {
         thread::sleep(std::time::Duration::from_secs(1));
     }
@@ -71,7 +67,7 @@ where
 {
     let mut samples = Vec::with_capacity(frame_size);
 
-    // Przygotowanie okna Hanninga
+    // Hanning window
     let window: Vec<f32> = (0..frame_size)
         .map(|i| {
             0.5 - 0.5 * (2.0 * std::f32::consts::PI * i as f32 / frame_size as f32).cos()
@@ -90,14 +86,12 @@ where
                     let sample: f32 = Sample::to_f32(&sample);
                     samples.push(sample);
                     if samples.len() == frame_size {
-                        //okno Hanninga
                         let windowed_samples: Vec<f32> = samples
                             .iter()
                             .zip(window.iter())
                             .map(|(&s, &w)| s * w)
                             .collect();
 
-                        // Najpierw znajduje najwlasciwszą metodę do fft a potem ją wykonuje
                         let mut planner = FftPlanner::new();
                         let fft = planner.plan_fft_forward(frame_size);
 
@@ -108,22 +102,17 @@ where
 
                         fft.process(&mut buffer);
 
-                        // Oblicza moduły fft 
-                        // Tylko pierwsza połowa bo potem według teorii jest odbicie symetryczne
                         let magnitudes_calc: Vec<f32> = buffer
                             .iter()
                             .take(frame_size / 2)
                             .map(|c| c.norm())
                             .collect();
 
-                        // Pobierz aktualne minimum dB
-                        // fachowo - threshold
                         let current_threshold = {
                             let thresh_lock = threshold.lock().unwrap();
                             *thresh_lock
                         };
 
-                        // Znajdź piki w widmie amplitud
                         let mut peak_indices = Vec::new();
                         for i in 1..(frame_size / 2 - 1) {
                             if magnitudes_calc[i] > magnitudes_calc[i - 1]
@@ -134,20 +123,15 @@ where
                             }
                         }
 
-                        // Znajdź najniższą częstotliwość piku (częstotliwość podstawową)
                         if let Some(&fundamental_index) = peak_indices.first() {
-                            // Początkowo program nie dawał zadowalających wyników
-                            // Ta funkcja interpolacji parabolicznej, okazała się być potrzebna
                             let alpha = magnitudes_calc[fundamental_index - 1];
                             let beta = magnitudes_calc[fundamental_index];
                             let gamma = magnitudes_calc[fundamental_index + 1];
                             let p = 0.5 * (alpha - gamma) / (alpha - 2.0 * beta + gamma);
                             let freq_bin = fundamental_index as f32 + p;
 
-                            // Oblicz częstotliwość
                             let freq = 2.0 * freq_bin * sample_rate as f32 / frame_size as f32;
 
-                            // Aktualizacja wartości freq wszędzie
                             {
                                 let mut freq_lock = frequency.lock().unwrap();
                                 *freq_lock = freq;
