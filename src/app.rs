7283a2d4b65eded91902e80d2c750b4e7ae30d68
Cyprian Zasada <120047588+cyptrix12@users.noreply.github.com>
2025-04-04 16:06:37 +0200

    Cleared comments

--- src/app.rs
+++ src/app.rs
@@ -7,38 +7,32 @@ pub struct App {
     pub threshold: Arc<Mutex<f32>>,
 }
 
-impl App { //Tutaj praktycznie całość implementacji wprost z dokumentacji
+impl App {
     pub fn new(
         frequency: Arc<Mutex<f32>>
     ) -> Self {
         Self {
             frequency,
-            threshold: Arc::new(Mutex::new(10.0)), // Domyślny próg na start
+            threshold: Arc::new(Mutex::new(10.0)),
         }
     }
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Pobierz aktualną częstotliwość
         let freq = {
             let freq_lock = self.frequency.lock().unwrap();
             *freq_lock
         };
 
-        // Pobierz aktualny próg
         let threshold = {
             let thresh_lock = self.threshold.lock().unwrap();
             *thresh_lock
         };
 
-        // Sprawdź co to za nuta
         let (note, octave) = closest_note_with_octave(freq);
         let note_octave = format!("{}{}", note, octave);
 
-        // Utwórz centralny panel
-        // Szczerze to wybrałem akurat ten panel z dokumentacji,
-        // ale pewnie lepiej by było to rozłożyć jakoś inaczej
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| {
                 ui.add_space(150.0);
@@ -62,8 +56,6 @@ impl eframe::App for App {
                 );
                 ui.add_space(20.0);
 
-                // Suwak do regulacji threshold
-                // Walczyłem żeby był na środku ale nie podołałem
                 ui.horizontal_centered(|ui| {
                     ui.label("Min dB:");
                     let mut thresh = threshold;
